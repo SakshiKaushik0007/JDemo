4f03dd5fd1c284c4cb51072c89dca3c5ba54e7e7
SakshiKaushik0007 <86341539+SakshiKaushik0007@users.noreply.github.com>
2021-11-26 01:50:02 +0530

    Add files via upload

---
+++ SumOfNumbers.java
@@ -0,0 +1,9 @@
+public class SumOfNumbers
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, n3=147, sum;  
+sum = n1 + n2 + n3;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  
