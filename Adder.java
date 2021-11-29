39f8735d53d8a4617cccd4e331274c238733785f
SakshiKaushik0007 <86341539+SakshiKaushik0007@users.noreply.github.com>
2021-11-29 11:26:46 +0530

    Update Adder.java

--- Adder.java
+++ Adder.java
@@ -1,9 +1,9 @@
-class Adder{  
+class TestOverloading1{  
 static int add(int a,int b){return a+b;}  
 static int add(int a,int b,int c){return a+b+c;}  
 }  
-class TestOverloading1{  
+class Adder{  
 public static void main(String[] args){  
-System.out.println(Adder.add(11,11));  
-System.out.println(Adder.add(11,11,11));  
+System.out.println(TestOverloading1.add(11,11));  
+System.out.println(TestOverloading1.add(11,11,11));  
 }}  
